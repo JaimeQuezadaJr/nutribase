4a125cdeb28dd54bdc4e48b1de3cab69ffc323be
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2022-09-02 20:22:24 -0700

    server code

---
+++ server/server.js
@@ -0,0 +1,28 @@
+//imports
+require ('./config/mongoose.config')
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const port = process.env.PORT || 4000;
+
+// create express app object
+const app = express();
+
+// express
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// cookie & jwt
+// app.use(cors()); //if without cookie-parse
+app.use(cookieParser());
+app.use(cors({ origin: 'http://localhost:3000', credentials: true}));
+
+// routes for login & registration
+const userRoutes = require('./routes/user.routes');
+userRoutes(app);
+
+//.. future routes
+
+
+app.listen(port, () => console.log(`Listening on port: ${port}`));
