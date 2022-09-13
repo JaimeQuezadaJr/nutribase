6705942701518bd9319aa892cfcbcb9fa243880d
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2022-09-12 20:56:37 -0700

    app js routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <BrowserRouter>
+        <Header loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
+        <Routes>
+          <Route path={'/'} element={<HomePage loggedIn={loggedIn} setLoggedIn={setLoggedIn} />} />
+          <Route path={'/login'} element={<UserLogin setLoggedIn={setLoggedIn} />} />
+          <Route path={'/register'} element={<UserRegistration setLoggedIn={setLoggedIn} />}/>
+          <Route path={'/about'} element={<About setLoggedIn={setLoggedIn} />} />
+          <Route path={'/dashboard/'} element={<GoalDashboard setLoggedIn={setLoggedIn} />} />
+          <Route path={'/goal/add/:category'} element={<GoalAdd setLoggedIn={setLoggedIn} />} />
+          <Route path={'/goal/edit/:category/:id'} element={<GoalUpdate setLoggedIn={setLoggedIn} />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
