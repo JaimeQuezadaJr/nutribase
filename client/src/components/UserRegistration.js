de8c0c6d4a2b4157a6f8ea415a5e5cfe844ec58f
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2022-09-17 20:40:21 -0700

    user registration element

--- client/src/components/UserRegistration.js
+++ client/src/components/UserRegistration.js
@@ -33,3 +33,53 @@ const UserRegistration = ({ setLoggedIn }) => {
       })
       .catch((err) => setErrors(err.response.data.errors));
   };
+
+  return (
+    <Form onSubmit={handleSubmit} className='form mb-5'>
+        <Container>
+          <Form.Group className="mb-3 col-md-4" controlId="firstName">
+              <Form.Label>First Name</Form.Label>
+              <Form.Control type="text" name='firstName' placeholder="Enter first name" value={user.firstName} onChange={handleChange} required />
+              {errors.firstName && <Form.Text className='text-danger'>{errors.firstName.message}</Form.Text>}
+            </Form.Group>
+            <Form.Group className="mb-3 col-md-4" controlId="lastName">
+              <Form.Label>Last Name</Form.Label>
+              <Form.Control type="text" name='lastName' placeholder="Enter last name" value={user.lastName} onChange={handleChange} required />
+              {errors.lastName && <Form.Text className='text-danger'>{errors.lastName.message}</Form.Text>}
+            </Form.Group>
+            <Form.Group className="mb-3 col-md-4" controlId="age">
+              <Form.Label>Age</Form.Label>
+              <Form.Control type="number" name='age' placeholder="Enter age" value={user.age} onChange={handleChange} required />
+              {errors.age && <Form.Text className='text-danger'>{errors.age.message}</Form.Text>}
+            </Form.Group>
+            <Form.Group className="mb-3 col-md-4" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" name='email' placeholder="Enter email" value={user.email} onChange={handleChange} required />
+              
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+              {errors.email && <Form.Text className='text-danger'><br></br> {errors.email.message}</Form.Text>}
+              
+            </Form.Group>
+            
+
+          <Form.Group className="mb-3 col-md-4" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" name='password' placeholder="Password" value={user.password} onChange={handleChange} />
+            {errors.password && <Form.Text className='text-danger'>{errors.password.message}</Form.Text>}
+          </Form.Group>
+          <Form.Group className="mb-3 col-md-4" controlId="formBasicConfirmPassword">
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control type="password" name='confirmPassword' placeholder="Must Match Password" value={user.confirmPassword} onChange={handleChange} />
+            {errors.confirmPassword && <Form.Text className='text-danger'>{errors.confirmPassword.message}</Form.Text>}
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Register
+          </Button>
+        </Container>
+      </Form>
+  );
+};
+
+export default UserRegistration;
