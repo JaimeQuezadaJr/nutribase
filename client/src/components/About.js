64f133439dd070c878d9d8f13c587333731c73f0
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2022-09-19 20:25:58 -0700

    about element

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -24,3 +24,19 @@ const About = ({setLoggedIn}) => {
     });
 
   },[])
+
+  return (
+    <Container className='form mb-5'>
+    <Card className='col-lg-6'>
+      <Card.Header as="h5">Become more in tune with yourself!</Card.Header>
+      <Card.Body>
+        <Card.Text>
+        Overall mental and physical well being should be at the top of anyone's priorities. Finding that balance will help you feel better. This platform facilitates your ability to track your nutrition, fitness, and mindfulness goals. It allows you to neatly organize your goals so you can commit to complete them by a certain date. Being able to track, commit, and achieve specific target objectives will allow you to be better in tune with yourself. The intent is to provide a platform where you can take some small steps to achieving your desired self equilibrium.
+        </Card.Text>
+        <Button variant="primary" onClick={() => navigate(btnLink[0])}>{btnLink[1]}</Button>
+      </Card.Body>
+    </Card>
+    </Container>
+  )
+}
+export default About
