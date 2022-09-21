6fec0f1fe778b78f52058feedb72806e8a6c4fa9
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2022-09-20 20:42:34 -0700

    home page element

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -34,3 +34,30 @@ const HomePage = ({loggedIn, setLoggedIn}) => {
     .then((result) => setQuote(result.contents.quotes[0]))
     .catch((err) => console.log(err))
   },[])
+
+  return (
+    <Container className='mt-5 mb-5'>
+      <Card className='col-lg-6 big-card'>
+        <Card.Img variant="top" src="https://media.istockphoto.com/photos/wellness-sign-with-wooden-cubes-picture-id513319180?b=1&k=20&m=513319180&s=170667a&w=0&h=vhYdVc-IHZLyS3xqNVuUTcBFpsdJRKPmLzFQpgKnpJY=" />
+        <Card.Body>
+          <Card.Title>Track | Commit | Achieve</Card.Title>
+          <Card.Text>
+            Commit yourself to achieving your maximum wellness goals!
+          </Card.Text>
+          <Button variant="primary" onClick={() => navigate(btnLink[0])}>{btnLink[1]}</Button>
+        </Card.Body>
+      </Card>
+      <Card className='col-lg-6 big-card'>
+        <Card.Img variant="top" src= {quote.background} />
+        <Card.Body>
+          <Card.Title>{quote.title}</Card.Title>
+          <Card.Text className='text-muted quote'>
+           " {quote.quote} "<br></br><span>- {quote.author}</span>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </Container> 
+  )
+}
+
+export default HomePage;
