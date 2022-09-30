bb5e4d20a6df0e73ceac065373d3762b8ca0b03c
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2022-09-29 20:23:21 -0700

    updated goal schema

--- server/models/goal.model.js
+++ server/models/goal.model.js
@@ -5,6 +5,16 @@ const GoalSchema = mongoose.Schema (
         maxDailyCalories: {
             type: Number,
             required: [true, 'Please enter desired max daily calories']
+        },
+        createdBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
         }
+    },
+    {
+        timestamps: true
     }
 )
+
+const Goal = mongoose.model('goal', GoalSchema);
+module.exports = Goal;
