ed099e9cf24888acaa38e992f9750ef6a9c8b353
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2022-09-29 20:21:02 -0700

    added goal schema

--- server/models/goal.model.js
+++ server/models/goal.model.js
@@ -1,2 +1,10 @@
 const mongoose = require('mongoose');
 
+const GoalSchema = mongoose.Schema (
+    {
+        maxDailyCalories: {
+            type: Number,
+            required: [true, 'Please enter desired max daily calories']
+        }
+    }
+)
