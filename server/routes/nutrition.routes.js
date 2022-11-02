dfbe9df54fd52fe7f134d5f897810a082065b83e
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2022-11-01 19:55:37 -0700

    re-added authentication in nutrition routes

--- server/routes/nutrition.routes.js
+++ server/routes/nutrition.routes.js
@@ -1,5 +1,5 @@
 const NutritionController = require('../controllers/nutrition.controller');
-// const { authenticate } = require('../config/jwt.config');
+const { authenticate } = require('../config/jwt.config');
 
 module.exports = (app) => { 
     app.get('/api/nutrition', NutritionController.findAllNutritions);
